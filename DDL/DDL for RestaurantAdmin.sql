1dbb1ab8ceccbdf265ceead445caea5735ef29e3
GriffinWilchuk <gwilchuk@student.ubc.ca>
2022-11-04 09:42:40 -0700

    Uploaded DDL files
    DDL files for Admin, Food, Restaurant, RestaurantAdmin.

---
+++ DDL/DDL for RestaurantAdmin.sql
@@ -0,0 +1,10 @@
+
+
+CREATE TABLE IF NOT EXISTS restaurantAdmin(
+	restaurantAdminID mediumint NOT NULL AUTO_INCREMENT,
+    restuarantAdminName varchar(100) NOT NULL,
+    restuarantAdminPass varchar(100) NOT NULL,
+    restaurantAdminPhone varchar(20) NOT NULL,
+    restaurantAdminEmail varchar(100) NOT NULL,
+    PRIMARY KEY (adminID)
+);
