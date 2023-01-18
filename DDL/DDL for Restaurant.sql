bfae3e8da8650fd2998cf1d2e1d2b9f4f1020dc6
Bist07 <74443096+Bist07@users.noreply.github.com>
2023-01-18 12:27:34 -0800

    changed varchar to text for storeDescriptions and address.
    switched datatype of columns, storeDescription and address.

--- DDL/DDL for Restaurant.sql
+++ DDL/DDL for Restaurant.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Restaurant (
   storeID mediumint NOT NULL AUTO_INCREMENT,
   storeName varchar(100) NOT NULL, 
-  storeDescription varchar(65000),
+  storeDescription text(65000),
   totalDonatedAmount int(255),
-  cuisineStyle varchar(65000),
-  address varchar(65000) NOT NULL,
+  cuisineStyle text(65000),
+  address text(65000) NOT NULL,
   openHours varchar (100), 
   restaurantImg varchar(100) NOT NULL,
   PRIMARY KEY (`storeID`)
