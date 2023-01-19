0863deb10d1c181dcb4995c45b3804966ab7a282
Bist07 <74443096+Bist07@users.noreply.github.com>
2023-01-19 13:53:51 -0800

    IT WORKS!

--- EquiFood-A-ReactNative/server.js
+++ EquiFood-A-ReactNative/server.js
@@ -8,25 +8,28 @@ const fs = require("fs");
 
 // 2.) Retrieve the Certificate Authority chain file (wherever you placed it - notice it's just in the Node project root here)
 const serverCert = [fs.readFileSync("\aws_skysql_chain.pem", "utf8")];
-var DB_HOST = "equifooda - db00009082.mdb0002659.db1.skysql.net";
-var DB_USER = "DB00009082";
-var DB_HOST = "B0w!W3Q[ovzdPZek7GvUG729";
-var DB_PORT = 5001;
-var DB_NAME = "equifooda";
+DB_HOST = "equifooda-db00009090.mdb0002659.db1.skysql.net";
+DB_USER = "DB00009090";
+DB_PASS = "bogiXUAS(ifEK254dnCUYVsN";
+DB_PORT = 5001;
+DB_NAME = "equifooda";
 
+console.log("lol");
 // Create a connection pool
 var pool =
     mariadb.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME//,
+        host: "equifooda-db00009090.mdb0002659.db1.skysql.net",//process.env.DB_HOST,
+        user: "DB00009090",//process.env.DB_USER,
+        password: "bogiXUAS(ifEK254dnCUYVsN",//process.env.DB_PASS,
+        port: 5001,//process.env.DB_PORT,
+        database: "equifooda",// process.env.DB_NAME//,
         // 3.) Add an "ssl" property to the connection pool configuration, using the serverCert const defined above
-        // ssl: {
-        //       ca: serverCert
-        // }
+        ssl: {
+            ca: serverCert
+        }
     });
+
+console.log("lol2");
 
 // Expose the Pool object within this module
 module.exports = Object.freeze({
