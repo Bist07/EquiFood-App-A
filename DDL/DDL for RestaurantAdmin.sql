eec45fa889301a311dbfb22f2d9634a86e0e5b3e
Bist07 <74443096+Bist07@users.noreply.github.com>
2023-01-23 13:48:37 -0800

    Update DDL for RestaurantAdmin.sql

--- DDL/DDL for RestaurantAdmin.sql
+++ DDL/DDL for RestaurantAdmin.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE IF NOT EXISTS restaurantAdmin(
-	restaurantAdminID mediumint NOT NULL AUTO_INCREMENT,
+	adminID mediumint NOT NULL,
     restuarantAdminName varchar(100) NOT NULL,
     restuarantAdminPass varchar(100) NOT NULL,
     restaurantAdminPhone varchar(20) NOT NULL,
