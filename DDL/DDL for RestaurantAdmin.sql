25cdda1314c56d14b28755ab658b597361dedd7e
GriffinWilchuk <113711473+GriffinWilchuk@users.noreply.github.com>
2023-01-27 10:11:35 -0800

    Changed adminID to Restaurant Admin

--- DDL/DDL for RestaurantAdmin.sql
+++ DDL/DDL for RestaurantAdmin.sql
@@ -1,11 +1,11 @@
 
 
 CREATE TABLE IF NOT EXISTS restaurantAdmin(
-	restaurantAdminID mediumint NOT NULL AUTO_INCREMENT,
+	restaurantAdminID mediumint NOT NULL,
     restuarantAdminName varchar(100) NOT NULL,
     restuarantAdminPass varchar(100) NOT NULL,
     restaurantAdminPhone varchar(20) NOT NULL,
     restaurantAdminEmail varchar(100) NOT NULL,
 	totalDonationPerRestaurant mediumint,
-    PRIMARY KEY (adminID)
+    PRIMARY KEY (restaurantAdminID)
 );
