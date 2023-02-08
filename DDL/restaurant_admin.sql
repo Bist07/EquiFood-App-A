ee6b236b91a1a7e31b77d37f969b862de73f90a1
Bist07 <74443096+Bist07@users.noreply.github.com>
2023-02-08 11:28:24 -0800

    Update restaurant_admin.sql

--- DDL/restaurant_admin.sql
+++ DDL/restaurant_admin.sql
@@ -1,10 +1,12 @@
 CREATE TABLE restaurant_admin(
-  PRIMARY KEY id INT NOT NULL AUTO_INCREMENT,
-  FOREIGN KEY restaurant_id INT REFERENCES restaurant(id),
+  id INT NOT NULL AUTO_INCREMENT,
+  restaurant_id INT,
   first_name VARCHAR(50) NULL DEFAULT NULL,
   last_name VARCHAR(50) NULL DEFAULT NULL,
   email VARCHAR(50) NULL DEFAULT NULL,
-  passwordHash VARCHAR(32) NOT NULL
+  passwordHash VARCHAR(32) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (restaurant_id) REFERENCES restaurant(id)
 );
 
 INSERT INTO restaurant_admin (restaurant_id, first_name, last_name, email, passwordHash)
