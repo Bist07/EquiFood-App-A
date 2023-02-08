6bc365fa1f734464de3f6d6beaa3a3bbcc1dcc56
Bist07 <74443096+Bist07@users.noreply.github.com>
2023-02-08 11:26:26 -0800

    Update food_order.sql

--- DDL/food_order.sql
+++ DDL/food_order.sql
@@ -1,10 +1,14 @@
 CREATE TABLE food_order (
-PRIMARY KEY id INT NOT NULL AUTO_INCREMENT,
-FOREIGN KEY customer_id INT REFERENCES customer(id),
-FOREIGN KEY restaurant_id INT REFERENCES restaurant(id),
-FOREIGN KEY order_status_id INT REFERENCES order_status(id),
+id INT NOT NULL AUTO_INCREMENT,
+customer_id INT,
+restaurant_id INT,
+order_status_id INT,
 Total_amount FLOAT NOT NULL DEFAULT 0,
 Reservation_datetime DATETIME NOT NULL,
 customer_rating TEXT NULL DEFAULT NULL,
-discount FLOAT NOT NULL DEFAULT 0
+discount FLOAT NOT NULL DEFAULT 0,
+PRIMARY KEY (id),
+FOREIGN KEY (customer_id) REFERENCES customer(id),
+FOREIGN KEY (restaurant_id) REFERENCES restaurant(id),
+FOREIGN KEY (order_status_id) REFERENCES order_status(id)
 );
