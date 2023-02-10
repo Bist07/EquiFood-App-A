44959ac9926f27eb7cac33e76ee32fb49c7c1b24
GriffinWilchuk <gwilchuk@student.ubc.ca>
2023-02-10 09:52:51 -0800

    Fixed customer query after backend changes

--- EquiFood-A-ReactNative/server/routes/customer.js
+++ EquiFood-A-ReactNative/server/routes/customer.js
@@ -2,14 +2,15 @@ const express = require('express');
 const pool = require('../helpers/database');  // define connection pool
 const router = express.Router();
 
-router.get('/:Username', async function(req,res){
+router.get('/', async function(req,res){
     try {
-        const sqlQuery = 'SELECT Username, Firstname, Lastname, Email, Password, created FROM Users WHERE Username=?'; //defines query
-        const rows = await pool.query(sqlQuery, req.params.Username); //rows = your returned query data
+        const sqlQuery = 'SELECT * FROM customer'; //defines query
+        const rows = await pool.query(sqlQuery, req.params.first_name); //rows = your returned query data
         console.log(rows.slice(rows.indexOf('}'))); // print row data minus all the fluff
         res.status(200).json(rows); //display row data on local host
      
     } catch (error) {
+
      res.status(400).send(error.message)
     }
 
@@ -22,6 +23,8 @@ router.post('/register',  async function(req,res){
     try {
        
         const {Email, Password} = req.body;
+
+        const encryptedPassword = await bcrypt.hash(Password, 10);
 
         const sqlQuery = 'INSERT INTO Users (Email, Password) VALUES (?,?)';
         const result = await pool.query(sqlQuery, [Email, Password]);
