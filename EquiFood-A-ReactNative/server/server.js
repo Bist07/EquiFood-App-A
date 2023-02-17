df1247b8e733552b00c42136327de88818262040
Bist07 <74443096+Bist07@users.noreply.github.com>
2023-02-17 11:36:17 -0800

    fixed merge conflict

--- EquiFood-A-ReactNative/server/server.js
+++ EquiFood-A-ReactNative/server/server.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (request, response) => {
     response.status(200).send("Hello world")
 })
+
 //creates connection router for Restaurant table
 const restaurantRouter = require('./routes/Restaurant');
 app.use('/Restaurant', restaurantRouter);
@@ -26,19 +27,6 @@ app.use('/Restaurant', restaurantRouter);
 //creates connection router for user table
 const customerRouter = require('./routes/customer');
 app.use('/customer', customerRouter);
-
-
-//creates connection router for menu_item table
-const menuRouter = require('./routes/Menu');
-app.use('/Menu', menuRouter);
-
-//creates connection router for admin table
-const adminRouter = require('./routes/admin');
-app.use('/admin', adminRouter);
-
-//creates connection router for restaurant_admin table
-const resadminRouter = require('./routes/resadmin');
-app.use('/admin', resadminRouter);
 
 
 /**
