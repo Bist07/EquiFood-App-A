55cf92d6ee7a9b6d1d82303adc719cf09fa54c1c
Bist07 <74443096+Bist07@users.noreply.github.com>
2023-02-25 17:29:40 -0800

    Delete config.js

--- EquiFood-A-ReactNative/config.js
+++
@@ -1,8 +0,0 @@
-export default config = {
-    // CHANGE TO A URL THAT WORKS FOR YOU 
-    // Ios can use localhost, android requires ipv4
-    local: {
-        url: "http://192.168.0.36",
-        port: "5001",
-    }
-}
