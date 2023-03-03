8996bf6b93d3ea3ece4c58e4f6ee6a7f02b69562
MajinJake <jakedaongam@gmail.com>
2023-03-02 17:24:24 -0800

    fix restaurantpage and readd config file

---
+++ EquiFood-A-ReactNative/config.js
@@ -0,0 +1,8 @@
+export default config = {
+    // CHANGE TO A URL THAT WORKS FOR YOU 
+    // Ios can use localhost, android requires ipv4
+    local: {
+        url: "http://localhost",
+        port: "5001",
+    }
+}
