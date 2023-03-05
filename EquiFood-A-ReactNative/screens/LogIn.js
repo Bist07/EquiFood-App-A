5b3b902ba54d625e13c77bfeecd3c1739af77eb9
Bist07 <74443096+Bist07@users.noreply.github.com>
2023-03-05 00:33:03 -0800

    wip

--- EquiFood-A-ReactNative/screens/LogIn.js
+++ EquiFood-A-ReactNative/screens/LogIn.js
@@ -1,18 +1,19 @@
-import React, {useState} from 'react'
-import { StyleSheet, View, Text, Image, Pressable, TouchableOpacity, TouchableHighlight, TextInput} from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, Image, Pressable, TouchableOpacity, TouchableHighlight, TextInput } from 'react-native'
 import Logo from '../assets/logos/Equifood_Logo.png'
 import CustomInput from '../components/CustomInput'
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
-import {MaterialIcons} from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import ImagePicker from '../components/ImagePicker';
 
 
-const LogIn = ({onPress, text}) => {
-    const [username, setUsername] = useState('');    
+const LogIn = ({ onPress, text }) => {
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const route = useRoute();
     const navigation = useNavigation();
-    
+
     // function for signing in:
 
     const onSignInPressed = () => {
@@ -33,53 +34,54 @@ const LogIn = ({onPress, text}) => {
 
 
     return (
-        <View style={styles.root}>            
- 
-            <Image style={ styles.logo}
-            source = {Logo} />
-            
+        <View style={styles.root}>
+
+            <Image style={styles.logo}
+                source={Logo} />
+            <ImagePicker>
+            </ImagePicker>
             {/* <Text>Equifood</Text> */}
-        
+
             {/* <CustomInput placeholder="Username" value= {username} setValue={setUsername} />
             <CustomInput placeholder="Password" value={password} setValue={setPassword} secureTextEntry ={true}/>
         */}
-            <View style={{flexDirection:'row', borderBottomColor:'#ccc', borderBottomWidth:1, paddingBottom:8, marginBottom:25}}>
-                <MaterialIcons name='email' size={20} color='#ccc' style={{marginRight:5}}/>
-                <TextInput placeholder = 'Email' style={{ flex:1, paddingVertical:0}} keyboardType="email-address" />
+            <View style={{ flexDirection: 'row', borderBottomColor: '#ccc', borderBottomWidth: 1, paddingBottom: 8, marginBottom: 25 }}>
+                <MaterialIcons name='email' size={20} color='#ccc' style={{ marginRight: 5 }} />
+                <TextInput placeholder='Email' style={{ flex: 1, paddingVertical: 0 }} keyboardType="email-address" />
             </View>
 
-            <View style={{flexDirection:'row', borderBottomColor:'#ccc', borderBottomWidth:1, paddingBottom:8, marginBottom:25}}>
-                <MaterialIcons name="lock" size={20} color='#ccc' style={{marginRight:5}}/>
-                <TextInput placeholder = 'Password' style={{ flex:1, paddingVertical:0}} secureTextEntry={true} />
+            <View style={{ flexDirection: 'row', borderBottomColor: '#ccc', borderBottomWidth: 1, paddingBottom: 8, marginBottom: 25 }}>
+                <MaterialIcons name="lock" size={20} color='#ccc' style={{ marginRight: 5 }} />
+                <TextInput placeholder='Password' style={{ flex: 1, paddingVertical: 0 }} secureTextEntry={true} />
             </View>
 
             <TouchableOpacity style={styles.signInButton}
-            onPress={() => navigation.navigate('RestaurantsView')}>
-                <Text style ={styles.signInText}>Sign In</Text>
+                onPress={() => navigation.navigate('RestaurantsView')}>
+                <Text style={styles.signInText}>Sign In</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.passwordButton}
-             onPress={() => navigation.navigate('ForgotPassword')}>
-                <Text style ={styles.passwordText}>Forgot Password</Text>
+                onPress={() => navigation.navigate('ForgotPassword')}>
+                <Text style={styles.passwordText}>Forgot Password</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.passwordButton}
-             onPress={() => navigation.navigate('Register')}>
-                <Text style ={styles.passwordText}>Register</Text>
+                onPress={() => navigation.navigate('Register')}>
+                <Text style={styles.passwordText}>Register</Text>
             </TouchableOpacity>
 
-            
+
             <TouchableOpacity style={styles.ROButton}
-             onPress={() => navigation.navigate('RestaurantOwnerLogIn')}>
-                <Text style ={styles.ROText}>Log In as Restaurant Owner</Text>
-            </TouchableOpacity>
-            
-            <TouchableOpacity style={styles.AdminButton}
-             onPress={() => navigation.navigate('AdminLogIn')}>
-                <Text style ={styles.AdminText}>Log In as Admin</Text>
+                onPress={() => navigation.navigate('RestaurantOwnerLogIn')}>
+                <Text style={styles.ROText}>Log In as Restaurant Owner</Text>
             </TouchableOpacity>
 
-        
+            <TouchableOpacity style={styles.AdminButton}
+                onPress={() => navigation.navigate('AdminLogIn')}>
+                <Text style={styles.AdminText}>Log In as Admin</Text>
+            </TouchableOpacity>
+
+
         </View>
     )
 }
@@ -89,60 +91,60 @@ export default LogIn;
 
 
 const styles = StyleSheet.create({
-    root:{
+    root: {
         alignItems: 'center',
         padding: 20,
         backgroundColor: 'white',
-        flex:1,
+        flex: 1,
         // borderTopLeftRadius:50,
         // borderTopRightRadius: 50,
         // borderBottomLeftRadius: 50,
         // borderBottomEndRadius: 50
     },
 
-    logo:{
+    logo: {
         // backgroundColor: "blue",
-        width: '100%', 
+        width: '100%',
         height: 150,
-        resizeMode : 'contain', 
-        marginTop: 120, 
+        resizeMode: 'contain',
+        marginTop: 120,
         borderRadius: 70,
         padding: 20,
         paddingBottom: 40,
-        marginBottom:50,
-  
+        marginBottom: 50,
+
     },
-    signInButton:{
+    signInButton: {
         backgroundColor: '#50c864',
         width: '100%',
         padding: 15,
         marginVertical: 5,
-        marginTop:20,
+        marginTop: 20,
 
         alignItems: 'center',
         borderRadius: 5
     },
-    signInText:{
-        fontWeight:'bold',
-        color:'white'
+    signInText: {
+        fontWeight: 'bold',
+        color: 'white'
     },
-    passwordButton:{
+    passwordButton: {
         marginTop: 13,
     },
-    passwordText:{
-        color:'grey',
+    passwordText: {
+        color: 'grey',
     },
-    ROButton:{
-        marginTop:30,
+    ROButton: {
+        marginTop: 30,
     },
-    ROText:{
+    ROText: {
         color: '#50c864',
         fontSize: 18,
     },
-    AdminButton:{
-        marginTop:20,
+    AdminButton: {
+        marginTop: 20,
     },
-    AdminText:{
+    AdminText: {
         color: '#50c864',
         fontSize: 18,
     }
